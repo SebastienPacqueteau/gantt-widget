fbf24baa97ecd4574bb6676472495a89ea5ce07b
Sébastien <sebastienpacqueteau@gmail.com>
2026-01-24 18:55:54 +0100

    creation des modules

--- js/gantt.js
+++ js/gantt.js
@@ -1,3 +1,61 @@
 // ==========================
 // ====== Module Gantt ======
 // ==========================
+
+class Gantt{
+  constructor(id){
+    this.element = document.getElementById(id);
+    this._width = 2000;
+    this._height = 20;
+    this._listeProjets = [];
+  }
+
+  get width(){
+    return this._width;
+  }
+
+  get height(){
+    return this._height;
+  }
+
+  get listeProjets(){
+    return this._listeProjets;
+  }
+
+  ajouter(projet){
+    this._listeProjets.push(projet);
+  }
+
+  set height(newHeight){
+    this._height = newHeight;
+  }
+
+  toString(){
+    //console.log(this, this.element);
+    return `Canvas ${this.element.id} : ${this._width} x ${this._height}`;
+  }
+
+  ajusterDimension(){
+    this.element.width = this._width;
+    this.element.height = this._height;
+  }
+
+  dessiner(listeTitres){
+    const ctx = this.element.getContext('2d');
+    ctx.fillStyle = 'red';
+    ctx.fillRect(20, 10, 1460, 20);
+    this.ligneTitres(listeTitres);
+  }
+
+  ligneTitres(listeTitres){
+    const ctx = this.element.getContext('2d');
+    ctx.fillStyle = 'blue';
+    listeTitres.forEach((titre, i) => {
+      ctx.fillText(titre, 20 + 200*i, 50);
+    });
+
+
+  }
+}
+
+export { Gantt };
--- js/widget.js
+++ js/widget.js
@@ -2,12 +2,48 @@
 // ====== script principale ======
 // ===============================
 
+import { Gantt } from './gantt.js';
+import { Config } from './config.js';
 
 
-function basculerPanneauOption() {
-		const panneauConfig = document.getElementById('panneauConfig');
-		panneauConfig.classList.toggle('collapsed');
+//Gantt
+const titreGantt = new Gantt('titreGantt');
+const gantt = new Gantt('gantt');
+
+//panneau de configuration
+const config = new Config();
+
+grist.ready({
+  onEditOptions() {
+    basculerPanneauConfiguration();
+  },
+  columns: config.colonnesNecessaires,
+  requiredAccess: 'read table',
+  allowSelectBy: false
+});
+
+(async function start(){
+  //configuration
+  await config.grist();
+  //console.log(config);
+
+  //Dessiner le Gantt
+  titreGantt.ajusterDimension();
+  gantt.height = 1500;
+  gantt.ajusterDimension();
+  gantt.dessiner(Array.from(config.colonnesG, obj => obj.titre));
+})();
+
+function afficherLesResultats(){
+  config.optionsPanneau();
 }
 
-document.getElementById('ouvrirConfig').addEventListener("click", basculerPanneauOption);
-document.getElementById('fermerConfig').addEventListener("click", basculerPanneauOption);
+function basculerPanneauConfiguration() {
+  const panneauConfig = document.getElementById('panneauConfig');
+  panneauConfig.classList.toggle('collapsed');
+}
+
+// liste des évènements
+document.getElementById('afficherLesResultats').addEventListener("click", afficherLesResultats);
+document.getElementById('ouvrirConfig').addEventListener("click", basculerPanneauConfiguration);
+document.getElementById('fermerConfig').addEventListener("click", basculerPanneauConfiguration);
